e70db25e96a5138ff84422dd68eb1f8b4f611989
Oleg Ryazancev <oleg.ryazancev1022@gmail.com>
2024-01-16 18:00:48 +0300

    Create purchase-history microservice and add MongoDB dependencies

---
+++ purchase-history/src/main/java/com/ryazancev/purchasehistory/PurchaseHistoryApplication.java
@@ -0,0 +1,13 @@
+package com.ryazancev.purchasehistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseHistoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PurchaseHistoryApplication.class, args);
+    }
+
+}
