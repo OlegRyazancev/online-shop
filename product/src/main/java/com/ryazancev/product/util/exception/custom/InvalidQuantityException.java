a02ed9d5029ba19b69c8fc1575616d028e2cecb1
Oleg Ryazancev <oleg.ryazancev1022@gmail.com>
2024-01-26 10:15:08 +0300

    Create exception handler in review microservice

--- product/src/main/java/com/ryazancev/product/util/exception/custom/InvalidQuantityException.java
+++ product/src/main/java/com/ryazancev/product/util/exception/custom/InvalidQuantityException.java
@@ -1,21 +1,20 @@
-package com.ryazancev.review.util.exception;
+package com.ryazancev.product.util.exception.custom;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
-
 @AllArgsConstructor
 @Getter
 @Setter
-public class ReviewCreationException extends RuntimeException{
+public class InvalidQuantityException extends RuntimeException {
 
     private HttpStatus httpStatus;
 
-
-    public ReviewCreationException(String message, HttpStatus httpStatus) {
+    public InvalidQuantityException(String message, HttpStatus httpStatus) {
         super(message);
         this.httpStatus = httpStatus;
     }
+
 }
