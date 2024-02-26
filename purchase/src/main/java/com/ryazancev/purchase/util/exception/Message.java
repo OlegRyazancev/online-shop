d2be2f58817e0387c4fd807e4f6b7075a46eedaf
Oleg Ryazancev <oleg.ryazancev1022@gmail.com>
2024-02-26 17:56:35 +0300

    Refactor messages in purchase microservice

--- purchase/src/main/java/com/ryazancev/purchase/util/exception/Message.java
+++ purchase/src/main/java/com/ryazancev/purchase/util/exception/Message.java
@@ -7,18 +7,17 @@ package com.ryazancev.purchase.util.exception;
 public class Message {
 
     public static final String CUSTOMER_SERVICE_UNAVAILABLE =
-            "The customer service is currently unavailable. " +
-                    "Please try again later";
-
-    public static final String PRODUCT_SERVICE_UNAVAILABLE =
-            "The product service is currently unavailable. " +
-                    "Please try again later";
-    public static final String PURCHASE_NOT_FOUND =
-            "Purchase not found";
+            "The customer service is currently unavailable. Please try again later";
 
     public static final String INSUFFICIENT_FUNDS =
-            "Customer doesn't have enough money to purchase the product";
-    public static final String NO_PRODUCTS_IN_STOCK =
-            "No available products in stock";
+            "The customer does not have enough funds to purchase the product";
 
+    public static final String NO_PRODUCTS_IN_STOCK =
+            "No products are currently available in stock";
+
+    public static final String PRODUCT_SERVICE_UNAVAILABLE =
+            "The product service is currently unavailable. Please try again later";
+
+    public static final String PURCHASE_NOT_FOUND =
+            "Purchase not found";
 }
