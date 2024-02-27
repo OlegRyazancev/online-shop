55d3b06f65dec217aeeea4b5129756978dc2be6d
Oleg Ryazancev <oleg.ryazancev1022@gmail.com>
2024-02-27 11:18:49 +0300

    Range Strings in Message.class in review microservice

--- review/src/main/java/com/ryazancev/review/util/exception/Message.java
+++ review/src/main/java/com/ryazancev/review/util/exception/Message.java
@@ -5,21 +5,18 @@ package com.ryazancev.review.util.exception;
  */
 
 public class Message {
-
-    public static final String PRODUCT_SERVICE_UNAVAILABLE =
-            "The product service is currently unavailable. " +
-                    "Please try again later";
+    public static final String DUPLICATE_REVIEW =
+            "A review with the same purchase ID already exists";
 
     public static final String CUSTOMER_SERVICE_UNAVAILABLE =
             "The customer service is currently unavailable. " +
                     "Please try again later";
 
+    public static final String PRODUCT_SERVICE_UNAVAILABLE =
+            "The product service is currently unavailable. " +
+                    "Please try again later";
+
     public static final String PURCHASE_SERVICE_UNAVAILABLE =
             "The purchase service is currently unavailable. " +
                     "Please try again later";
-
-    public static final String DUPLICATE_REVIEW =
-            "A review with the same purchase ID already exists";
-
-
 }
