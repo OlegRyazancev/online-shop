2ec0d209b0103247bfad98eaf201e46e107188fb
Oleg Ryazancev <oleg.ryazancev1022@gmail.com>
2024-02-27 13:12:02 +0300

    Add @author to Eureka-server classes

--- eureka-server/src/main/java/com/ryazancev/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/ryazancev/eurekaserver/EurekaServerApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author Oleg Ryazancev
+ */
+
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
