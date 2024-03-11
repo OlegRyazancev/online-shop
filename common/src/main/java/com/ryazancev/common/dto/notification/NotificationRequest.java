ca176324bc5246889a1d957446db381270481ce7
Oleg Ryazancev <oleg.ryazancev1022@gmail.com>
2024-03-11 12:09:31 +0300

    Add kafka consumer config

---
+++ common/src/main/java/com/ryazancev/common/dto/notification/NotificationRequest.java
@@ -0,0 +1,23 @@
+package com.ryazancev.common.dto.notification;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+/**
+ * @author Oleg Ryazancev
+ */
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Schema(description = "Notification request")
+public class NotificationRequest {
+
+    private Long recipientId;
+
+    private Long senderId;
+
+    private NotificationType type;
+}
---
+++ notification/src/main/java/com/ryazancev/notification/kafka/config/NotificationConsumerConfig.java
@@ -0,0 +1,71 @@
+package com.ryazancev.notification.kafka.config;
+
+import com.ryazancev.common.dto.notification.NotificationRequest;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Oleg Ryazancev
+ */
+
+@Configuration
+public class NotificationConsumerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.consumer.group-id}")
+    private String groupId;
+
+    public Map<String, Object> baseConsumerConfig() {
+
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+
+        return props;
+    }
+
+    @Bean
+    public ConsumerFactory<String, NotificationRequest> consumerFactory() {
+
+        JsonDeserializer<NotificationRequest> jsonDeserializer =
+                new JsonDeserializer<>();
+
+        jsonDeserializer.addTrustedPackages("*");
+
+        return new DefaultKafkaConsumerFactory<>(
+                baseConsumerConfig(),
+                new StringDeserializer(),
+                jsonDeserializer
+        );
+    }
+
+    @Bean
+    KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<
+            String, NotificationRequest>>
+    messageFactory(ConsumerFactory<
+            String, NotificationRequest> consumerFactory) {
+
+        ConcurrentKafkaListenerContainerFactory<
+                String, NotificationRequest> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+
+        factory.setConsumerFactory(consumerFactory);
+
+        return factory;
+    }
+
+}
