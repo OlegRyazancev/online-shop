11e3a11469c72458384ac281da5cd9cddd3bc2aa
Oleg Ryazancev <oleg.ryazancev1022@gmail.com>
2024-03-11 19:03:59 +0300

    Optimize imports in WebSocketConfig

--- notification/src/main/java/com/ryazancev/notification/config/WebSocketConfig.java
+++ notification/src/main/java/com/ryazancev/notification/config/WebSocketConfig.java
@@ -1,17 +1,10 @@
 package com.ryazancev.notification.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.converter.DefaultContentTypeResolver;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
-import java.util.List;
 
 /**
  * @author Oleg Ryazancev
