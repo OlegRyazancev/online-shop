84ea02dfe2c9c032c21bf19d043a29fd91e6ba3f
Oleg Ryazancev <oleg.ryazancev1022@gmail.com>
2024-03-24 12:52:29 +0300

    Make Async methods in Auth Microservice

--- auth/src/main/java/com/ryazancev/auth/util/processor/KafkaMessageProcessor.java
+++ auth/src/main/java/com/ryazancev/auth/util/processor/KafkaMessageProcessor.java
@@ -5,6 +5,8 @@ import com.ryazancev.auth.model.ConfirmationToken;
 import com.ryazancev.auth.model.User;
 import com.ryazancev.common.dto.mail.MailDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,19 +15,29 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class KafkaMessageProcessor {
 
     private final DtoProcessor dtoProcessor;
     private final AuthProducerService authProducerService;
 
+    @Async("asyncTaskExecutor")
     public void sendRegistrationMailToCustomer(final ConfirmationToken token) {
 
+        log.info("Method sendRegistrationMailToCustomer starts work "
+                + "at thread: " + Thread.currentThread().getName());
+
         MailDto mailDto = dtoProcessor.createRegistrationMailDto(token);
+
         authProducerService.sendMessageToMailTopic(mailDto);
     }
 
+    @Async("asyncTaskExecutor")
     public void sendConfirmationMailToCustomer(final User user,
                                                final ConfirmationToken token) {
+
+        log.info("Method sendConfirmationMailToCustomer starts work "
+                + "at thread: " + Thread.currentThread().getName());
 
         MailDto mailDto = dtoProcessor.createConfirmationMailDto(
                 user.getEmail(),
